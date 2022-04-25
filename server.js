900a3e2ad0422187a4b381cfc175630e2147f68d
Bartosz Wilki <b.wilki0109@gmail.com>
2022-04-25 14:34:42 +0200

    prepare heroku v.5

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-app.use((req, res) => {
+/*app.use((req, res) => {
   res.status(404).send({message: '404 not found...'});
-})
+})*/
 
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
