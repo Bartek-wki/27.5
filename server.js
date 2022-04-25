a9289870265fe57133ff187a44ee17c75eef0c5c
Bartosz Wilki <b.wilki0109@gmail.com>
2022-04-25 14:41:50 +0200

    prepare heroku v.6

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -13,8 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '/client/build')));
-
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
@@ -23,9 +23,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-/*app.use((req, res) => {
+app.use((req, res) => {
   res.status(404).send({message: '404 not found...'});
-})*/
+})
 
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
